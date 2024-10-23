a55cfc094663feeba132217e9b54873053d388cf
LJB <woqls4@gmail.com>
2024-10-24 01:55:09 +0900

    chore: 이건 써도 된다

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ export default tseslint.config(
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'off',
       'no-unused-vars': 'off',
       'prettier/prettier': 'error',
     },
